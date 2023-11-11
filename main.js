7f5547a6fce45d283cb4edf2297ade885b22bb6e
WasdKami <110654379+wasdKami@users.noreply.github.com>
2023-11-11 13:05:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-const timeSelector = document.getElementsByTagName("select");
+        // Function to populate the time options
+        function populateTimeOptions() {
+            var select = document.getElementById("timeSelect");
 
-timeSelector.forEach(element => {
-    p.appendChild(document.createElement("p"));
-});
+            for (var hour = 0; hour <= 23; hour++) {
+                for (var minute = 0; minute < 60; minute += 15) {
+                    var option = document.createElement("option");
+                    option.value = hour.toString().padStart(2, '0') + ":" + minute.toString().padStart(2, '0');
+                    option.text = hour.toString().padStart(2, '0') + ":" + minute.toString().padStart(2, '0');
+                    select.add(option);
+                }
+            }
+        }
+
+        // Call the function to populate options on page load
+        populateTimeOptions();
